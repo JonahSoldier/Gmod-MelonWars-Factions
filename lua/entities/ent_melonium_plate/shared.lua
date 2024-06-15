91b3d92b005c9b7d76428e26a2fc20342f85cfd5
JonahSoldier <jonahmasonmerewether@gmail.com>
2024-06-15 19:46:29 +0100

    Editable Melonium-Plate Model
    Not much more to add that isn't in the title already.
    
    Previously I relied on model-manipulator to let people change the model of this entity. That's another "Fine for a small community, not fine for the workshop" sort of thing. I can't expect nearly that amount of ingenuity from the general public, so this is a solution that should work well-enough for at least the people who are invested enough to use it in their prop maps.
    
    Also I hate editable entities. They're so jank to work with and every time I use them I feel like I'm committing some sort of programming sin with the things I have to do to get them working how I want them.

--- lua/entities/ent_melonium_plate/shared.lua
+++ lua/entities/ent_melonium_plate/shared.lua
@@ -1,3 +1,5 @@
+AddCSLuaFile()
+
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
@@ -5,8 +7,29 @@ ENT.Category = "MelonWars"
 
 ENT.PrintName = "Melonium Plate"
 ENT.Author = "JonahSoldier"
-ENT.Contact = "don`t"
-ENT.Purpose = "Annoy"
-ENT.Instructions = "Spawn a whole bunch"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
+ENT.Editable = true
+
+
+function ENT:SetupDataTables()
+	self:NetworkVar( "String", 0, "MWModel", {
+		 KeyName = "mwModel",
+		 Edit = {
+			title = "Model",
+			type = "Generic"
+			--waitforenter = true --For some reason this makes it require you to input the text twice. Using SetModel without a valid model doesn't seem to cause issues so we're using that.
+		}
+	} )
+
+	if SERVER then
+		self:NetworkVarNotify( "MWModel", function(name, old, new)
+			self:SetModel(new)
+			self:PhysicsInit( SOLID_VPHYSICS )
+			self:GetPhysicsObject():EnableMotion(false)
+		end )
+		timer.Simple(0, function()
+			self:SetMWModel(self:GetModel())
+		end)
+	end
+end
