00ad1ced718c97b14902f6611d463de7e4ce8dda
JonahSoldier <jonahmasonmerewether@gmail.com>
2024-06-22 19:31:05 +0100

    particleBeam refactor
    self-explanatory.

--- lua/entities/ent_melonbullet_particlebeamtracer/init.lua
+++ lua/entities/ent_melonbullet_particlebeamtracer/init.lua
@@ -6,66 +6,59 @@ include( "shared.lua" )
 PrecacheParticleSystem("explosion_huge_c")
 
 function ENT:Initialize()
-
 	self:SetModel("models/hunter/misc/sphere025x025.mdl")
 	self:SetMaterial("models/props_combine/stasisshield_sheet")
-	self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
-	self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
-	self:SetMoveType(MOVETYPE_VPHYSICS)
 
 	self.deathSound = "ambient/explosions/explode_9.wav"
 
 	self.careForFriendlyFire = false
 	self.speed = 100
 	self.damageDeal = 250
-	self.maxHP = 20
-	self.random = Vector(0.0, 0.0, 0.0)
-	self.targetPos = Vector(0,0,0)
-	self.distance = 0
+	self.targetPos = vector_origin
 
 	self:SetColor(Color(100,100,100, 255))
-	local trail = util.SpriteTrail(self, 0, Color(255,255,255), true, 10, 1, 2000, 1/(15+1)*0.5, "sprites/tp_beam001")
+	util.SpriteTrail(self, 0, color_white, true, 10, 1, 2000, 1 / 32, "sprites/tp_beam001")
 
 	self.mwBulletIndestructible = true
 end
 
 function ENT:Think()
-	local foundEnts = ents.FindInSphere(self:GetPos(), 2 )
-	for k, v in pairs( foundEnts ) do
-		if (v.Base == "ent_melon_prop_base") then --si es una sandía
-			if (v:GetNWInt("mw_melonTeam", 0) ~= self:GetNWInt("mw_melonTeam", 0)) then-- si tienen distinto equipo
-				self:Explode()
-				return true
-			end
+	local selfTeam = self:GetNWInt("mw_melonTeam", -1)
+	local selfPos = self:GetPos()
+	for i, v in ipairs( ents.FindInSphere(selfPos, 2 ) ) do
+		if v.Base == "ent_melon_prop_base" and not MelonWars.sameTeam(selfTeam, v:GetNWInt("mw_melonTeam", -1)) then
+			self:Explode()
+			return
 		end
 	end
 
 	local target = self:GetNWEntity("target", nil)
-	if (self.targetPos == Vector(0,0,0) and target ~= nil and IsValid(target)) then
+	if self.targetPos == vector_origin and IsValid(target) then
 		self.targetPos = target:GetPos()
 	end
-
-	self.distance = self:GetPos():Distance(self.targetPos)
-	if (self.targetPos == Vector(0,0,0)) then self:Remove() end
-	local targetVec = self.targetPos+self.random*self.distance
-	self:SetPos(self:GetPos()+(targetVec-self:GetPos()):GetNormalized()*self.speed)
-	self:SetAngles( (targetVec-self:GetPos()):Angle() )
-	self:NextThink(CurTime()+0.05)
-	if (self.distance < self.speed*1.5) then
-		if (self:IsValid()) then
-			self:SetPos(self.targetPos)
-			self:Explode()
-		end
+	if self.targetPos == vector_origin then
+		self:Remove()
+		return
 	end
 
+	local targetVec = self.targetPos
+	local dirVec = (targetVec-selfPos)
+	self:SetPos( selfPos + dirVec:GetNormalized() * self.speed )
+	self:SetAngles( dirVec:Angle() )
+
+	local distSqr = selfPos:DistToSqr(self.targetPos)
+	if distSqr < (self.speed * 1.5) ^ 2 and self:IsValid() then
+		self:SetPos(self.targetPos)
+		self:Explode()
+	end
+
+	self:NextThink(CurTime() + 0.05)
 	return true
 end
 
 function ENT:Explode()
-
-
 	timer.Simple( 12.5, function()
-		if (self:IsValid()) then
+		if not self:IsValid() then return end
 			util.BlastDamage( self, self, self:GetPos(), 450, self.damageDeal )
 
 			local effectdata = EffectData()
@@ -79,6 +72,5 @@ function ENT:Explode()
 				sound.Play( "k_lab2.Barney_Explosion", v:GetPos(), 75, 100, 1 )
 			end
 			self:Remove()
-			end
 		end	)
 end
