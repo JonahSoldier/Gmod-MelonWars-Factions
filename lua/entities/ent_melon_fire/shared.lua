5be3fd0a2a632f6753f9209d156c09c5317eb239
JonahSoldier <jonahmasonmerewether@gmail.com>
2024-06-22 19:48:18 +0100

    Removed un-necessary spawnmenu data from entity shared files
    Removed unnecessary shared values (e.g. Contact, Author, Instructions, Category, etc) from non-spawnable unit entities

--- lua/entities/ent_melon_fire/shared.lua
+++ lua/entities/ent_melon_fire/shared.lua
@@ -1,12 +1,5 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.Category = "MelonWars"
-
-ENT.PrintName = "Death Fire"
-ENT.Author = "Marum"
-ENT.Contact = ""
-ENT.Purpose = "Any melon entity nearby will be set on fire"
-ENT.Instructions = ""
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
--- lua/entities/ent_melonbullet_bomb/shared.lua
+++ lua/entities/ent_melonbullet_bomb/shared.lua
@@ -1,12 +1,5 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.Category = "Marums pack"
-
-ENT.PrintName = "Melon Mortar Bomb"
-ENT.Author = "Marum"
-ENT.Contact = "boom"
-ENT.Purpose = "boom"
-ENT.Instructions = "boom"
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
