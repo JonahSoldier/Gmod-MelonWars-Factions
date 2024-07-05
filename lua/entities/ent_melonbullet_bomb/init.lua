de89991d87f49febdfad5092ff0c5904974badf6
JonahSoldier <jonahmasonmerewether@gmail.com>
2024-07-05 07:57:10 +0100

    Mortar tweaks
    Explode after hitting an ent_melon_base entity

--- lua/entities/ent_melonbullet_bomb/init.lua
+++ lua/entities/ent_melonbullet_bomb/init.lua
@@ -10,12 +10,8 @@ function ENT:Initialize()
 	local time = 3.7
 	self:Ignite( time, 0.1 )
 	timer.Simple( time, function()
-		if (self:IsValid()) then
-			util.BlastDamage( self, self, self:GetPos(), 30, 75 )
-			local effectdata = EffectData()
-			effectdata:SetOrigin( self:GetPos() )
-			util.Effect( "Explosion", effectdata )
-			self:Remove()
+		if self:IsValid() then
+			self:Explode()
 		end
 	end	)
 end
@@ -26,15 +22,26 @@ function ENT:PhysicsUpdate(colData, collider)
 	phys:ApplyForceCenter( vector_up * (phys:GetMass() * 5.6) )
 end
 
-function ENT:PhysicsCollide( colData, collider )
-	local other = colData.HitEntity
-	if not self:IsValid() then return end
-	if not(not other.canMove or other.Base == "ent_melon_prop_base") or not other:IsValid() then return end
-
-	other:TakeDamage(30, self, self)
-
+function ENT:Explode()
+	util.BlastDamage( self, self, self:GetPos(), 30, 75 )
 	local effectdata = EffectData()
 	effectdata:SetOrigin( self:GetPos() )
 	util.Effect( "Explosion", effectdata )
 	self:Remove()
+end
+
+function ENT:PhysicsCollide( colData, collider )
+	local other = colData.HitEntity
+	if not self:IsValid() or not other:IsValid() then return end
+
+	if (other.Base == "ent_melon_base" and not other.canMove) or other.Base == "ent_melon_prop_base" then
+		other:TakeDamage(30, self, self)
+
+		local effectdata = EffectData()
+		effectdata:SetOrigin( self:GetPos() )
+		util.Effect( "Explosion", effectdata )
+		self:Remove()
+	elseif other.Base == "ent_melon_base" then 
+		self:Explode()
+	end
 end
